5fad767e92d97d242b02e2ceb920e9299da4f90f
TheRedLady <christina.hristakieva@gmail.com>
2016-07-12 17:08:04 +0300

    new version of linkedlist.py

--- 02-data-modeling/linkedlist.py
+++ 02-data-modeling/linkedlist.py
@@ -1,6 +1,56 @@
+
+class Node(object):
+
+    def __init__(self, value, nxt):
+        self.value = value
+        self.next = nxt
+
+
+def Cons(val, nxt):
+    if nxt == nil:
+        return List(Node(val, nil))
+    return List(Node(val, nxt.head))
+
+nil = Node(0, None)
+
+
+class List(object):
+
+    def __init__(self, start):
+        self.head = start
+
+    @classmethod
+    def create(cls, *args):
+        list_obj = List(nil)
+        for item in args[::-1]:
+            prev = list_obj.head
+            list_obj.head = Node(item, prev)
+        return list_obj
+
+    def get_list(self):
+        lst = []
+        current = self.head
+        while current != nil:
+            lst.append(current.value)
+            current = current.next
+        return lst
+
+    def __iter__(self):
+        current = self.head
+        while current != nil:
+            yield current.value
+            current = current.next
+
+    def __getitem__(self, index):
+        return self.get_list()[index]
+
+    def __repr__(self):
+        lst = self.get_list()
+        return "<" + str(lst)[1:-1] + ">"
+
+
 """
 Creation
-
 >>> l = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, nil)))))
 >>> l
 <1, 2, 3, 4, 5>
@@ -9,49 +59,16 @@ Creation
 <1, 2, 3, 4, 5>
 
 Iteration
-
 >>> [li + 1 for li in l]
 [2, 3, 4, 5, 6]
 
 Indexing:
-
 >>> l[1]
 2
 >>> l[-2]
 4
 
 """
-
-
-class List:
-
-    def __init__(self,*args):
-        self.list = list(args)
-        self.isNil = self.list==[]
-
-    @classmethod
-    def create(cls, *args):
-        return List(*args)
-
-    def __iter__(self):
-        for item in self.list:
-            yield item
-
-    def __getitem__(self, index):
-        return self.list[index]
-
-    def __repr__(self):
-        return "<" + str(self.list)[1:-1] + ">"
-
-
-
-def Cons(fst, snd):
-    args = [fst]
-    if hasattr(snd, 'isNil'):
-        args.extend(snd.list)
-    else:
-        args.append(snd)
-    return List.create(*args)
 
 
 if __name__ == "__main__":
--- 02-data-modeling/testlinkedlist.py
+++ 02-data-modeling/testlinkedlist.py
@@ -1,24 +1,26 @@
 import unittest
-import linkedlist as l
+import linkedlist as llist
 
 class TestList(unittest.TestCase):
 
     def testStr(self):
-        tst = l.List(1, 2, 3)
+        tst = llist.List.create(1, 2, 3)
         self.assertEqual(str(tst), "<1, 2, 3>")
-        tst = l.List()
+        tst = llist.List(llist.nil)
         self.assertEqual(str(tst), "<>")
+        constr = llist.Cons(1, llist.Cons(2, llist.Cons(3, llist.nil)))
+        self.assertEqual(str(constr), "<1, 2, 3>")
 
     def testCreate(self):
-        t =l.List.create(4,5,6)
-        g = l.List(1,2,3)
-        nil = l.List()
-        self.assertEqual(t.list, [4,5,6])
-        self.assertEqual(g.list, [1,2,3])
-        self.assertEqual(nil.list, [])
+        t = llist.List.create(4,5,6)
+        g = llist.List.create(1,2,3)
+        constr = llist.Cons(1, llist.Cons(2, llist.Cons(3, llist.nil)))
+        self.assertEqual(constr.get_list(), [1,2,3])
+        self.assertEqual(t.get_list(), [4,5,6])
+        self.assertEqual(g.get_list(), [1,2,3])
 
     def testIndexing(self):
-        tst = l.List(0,1,2,3,4,5,6,7,8,9)
+        tst = llist.List.create(0,1,2,3,4,5,6,7,8,9)
         self.assertEqual(tst[2], 2)
         self.assertEqual(tst[-1], 9)
         self.assertEqual([item for item in tst], [0,1,2,3,4,5,6,7,8,9])
