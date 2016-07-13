bf15a06a5035be7bbcadc322518e6628a40e7ca7
Yavor Atov <yavor.atov@gmail.com>
2016-07-13 13:58:54 +0300

    Added some hints for 'Simple ORM' task

--- 04-metaprogramming/simple_orm.py
+++ 04-metaprogramming/simple_orm.py
@@ -1,2 +1,41 @@
-class Model(object):
+class Field(object):
+    def __init__(self):
+        pass
+
+    def __get__(self, obj, type=None):
+        pass
+
+    def __set__(self, obj, value):
+        pass
+
+
+class IntField(Field):
     pass
+
+
+class CharField(Field):
+    pass
+
+
+class ModelMetaclass(type):
+
+    def __new__(metaclass, classname, bases, class_dict):
+        pass
+
+
+class Model(object):
+
+    __metaclass__ = ModelMetaclass
+
+    id = IntField()
+    id.db_type = 'INTEGER PRIMARY KEY AUTOINCREMENT'
+
+    @classmethod
+    def setup_schema(cls, cursor):
+        pass
+
+    def __init__(self, cursor, **kwargs):
+        pass
+
+    def save(self):
+        pass
