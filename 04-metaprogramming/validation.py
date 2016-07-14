8f8b0c9d5478dfb6de0d5e8c28b52f5145e751c0
TheRedLady <christina.hristakieva@gmail.com>
2016-07-14 14:41:27 +0300

    Add validation task and tests

--- 04-metaprogramming/validation.py
+++ 04-metaprogramming/validation.py
@@ -1,3 +1,5 @@
+import numbers
+
 """
 @validate(
     arg('x', [number, lambda x: 0 < x]),
@@ -9,20 +11,34 @@ def calc(x, y):
 
 
 def is_number(n):
-    pass
+    return is_instanceof(numbers.Number)(n) # return isinstance(n, numbers.Number)
 
 
 def is_string(n):
-    pass
+    return is_instanceof(str)(n) # return isinstance(n, str)
 
 
 def is_instanceof(type):
-    pass
+    return lambda arg: isinstance(arg, type)
 
 
 def arg(name, validators):
-    pass
+    def func_validate(arg_dict):
+        for val in validators:
+            if not val(arg_dict[name]):
+                return False
+        return True
+    return func_validate
 
 
-def validate(validators):
-    pass
+def validate(*validators):
+    def decorator(f):
+        def check(*args):
+            arg_dict =  f.__code__.co_varnames
+            arg_dict = {name: value for name, value in zip(arg_dict, args)}
+            for item in validators:
+                if not item(arg_dict):
+                    raise TypeError("Invalid arguments")
+            return f(*args)
+        return check
+    return decorator
