6909399dc36edd90864da74f48aeaeb084a94ac6
Yavor Atov <yavor.atov@gmail.com>
2016-07-20 10:49:56 +0300

    Added tmux config

--- tools/vim/install-ycm.sh
+++ tools/vim/install-ycm.sh
@@ -3,7 +3,8 @@
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
 
+cd ~/.vim/bundle
 git clone https://github.com/Valloric/YouCompleteMe.git
-cd ~/.vim/bundle/YouCompleteMe
+cd YouCompleteMe
 git submodule update --init --recursive
 ./install.py
